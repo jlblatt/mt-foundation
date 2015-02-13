7e282a848d13f0c6fc3e7e0748d653d2e7a8aa33
jlblatt <jlblatt+github@gmail.com>
2015-02-12 21:40:14 -0500

    Data scraper

--- mt/conf/testdata/_generate/parseDiscogsData.php
+++ mt/conf/testdata/_generate/parseDiscogsData.php
@@ -5,6 +5,8 @@
 $artists = array(246650, 158120, 81013, 82730, 67156, 253281, 1289, 10263, 50183, 110593, 252354, 251595);
 $artists = array(158120);
 
+$sql = "";
+
 //api functions
 
 function getReleasePage($artist, $page)
@@ -27,6 +29,17 @@ function getAlbum($id)
   return $album; 
 }
 
+function getImage($url)
+{
+  $ext = preg_replace("/.*(\.\w\w\w\w?)/", "$1", $url);
+  $ch = curl_init($url . "?token=xxxxxxxxxxxxxxxxxxx");
+  $fp = fopen('images/' . mt_rand() . $ext, 'w');
+  curl_setopt($ch, CURLOPT_USERAGENT, 'MTFoundationTestDataCollector/1.0 +https://github.com/jlblatt/mt-foundation');
+  curl_setopt($ch, CURLOPT_FILE, $fp);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_exec($ch); curl_close($ch); fclose($fp);
+}
+
 
 
 
@@ -45,11 +58,11 @@ foreach($artists as $artist)
     {
       if($thisRelease->type == "master" && $thisRelease->role == "Main")
       {
-        $allAlbums[$artist][] = $thisRelease;
+        $allAlbums[$artist][] = getAlbum($thisRelease->id);
       }
     }
 
-    if(property_exists($thisSetOfReleases->pagination->urls, "next"))
+    if(property_exists($thisSetOfReleases->pagination->urls, "next") && false)
     {
       $page++;
       sleep(2);
@@ -65,6 +78,31 @@ foreach($artists as $artist)
 
 
 
+//download images
+foreach($allAlbums as $artist => $albums)
+{
+  foreach($albums as $album)
+  {
+    if(property_exists($album, "images") && count($album->images))
+    {
+      $prim = 0;
+      foreach($album->images as $index => $image)
+      {
+        if($image->type == "primary")
+        {
+          $prim = $index;
+        }
+      }
+      print_r($album->images);
+      getImage($album->images[$prim]->resource_url);
+      sleep(2);  
+    }
+  }
+}
+
+
+
+
 
 //build songs array
 $allSongs = array();
@@ -73,13 +111,66 @@ foreach($allAlbums as $artist => $albums)
 {
   foreach($albums as $album)
   {
-    $thisAlbum = getAlbum($album->id);
-
-    //stopped here
-    //  need to loop over track listing and push onto songs array
-    //  need to write out sql
-    //  need to download images
+    foreach($album->tracklist as $num => $song)
+    {
+      $song->num = $num + 1;
+      $song->album_id = $album->id;
+      $allSongs[] = $song;
+    }
 
     sleep(2);
   }
 }
+
+
+
+
+
+//generate sql
+$sql .= "# albums ##################################\n\n";
+foreach($allAlbums as $artist => $albums)
+{
+  foreach($albums as $album)
+  {
+    $sql .= sprintf("insert into {{{prefix}}}albums (id, title, image, thumbnail, pubyear, artist_id) values (%u, '%s', '%s', '%s', %u, %u);\n",
+      $album->id,
+      sqlescape($album->title),
+      sqlescape(''),
+      sqlescape(''),
+      $album->year,
+      $artist
+    );
+  }
+}
+
+$sql .= "\n# songs ###################################\n\n";
+foreach($allSongs as $song)
+{
+  $sql .= sprintf("insert into {{{prefix}}}songs (title, track_no, album_id) values ('%s', %u, %u);\n",
+    sqlescape($song->title),
+    $song->num,
+    $song->album_id
+  );
+}
+
+echo $sql;
+
+
+
+
+
+//mysql_real_escape_string replacement since we aren't worried about injection
+//http://evertpot.com/escaping-mysql-strings-with-no-connection-available/
+function sqlescape($unescaped) {
+  $replacements = array(
+     "\x00"=>'\x00',
+     "\n"=>'\n',
+     "\r"=>'\r',
+     "\\"=>'\\\\',
+     "'"=>"\'",
+     '"'=>'\"',
+     "\x1a"=>'\x1a'
+  );
+  return strtr($unescaped,$replacements);
+}
+
