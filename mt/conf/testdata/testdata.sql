12d067313bdaddfe657be8d94d3cfe7bbf1ef3fa
jlblatt <jlblatt+github@gmail.com>
2015-04-16 19:31:21 -0400

    Ratatat

--- mt/conf/testdata/testdata.sql
+++ mt/conf/testdata/testdata.sql
@@ -47,3 +47,61 @@ insert into {{{prefix}}}artists (id, name, description, image, date_created, dat
 insert into {{{prefix}}}artists (id, name, description, image, date_created, date_modified) values (110593, 'Jimi Hendrix', 'James Marshall "Jimi" Hendrix (born Johnny Allen Hendrix; November 27, 1942 - September 18, 1970) was an American musician, guitarist, singer, and songwriter. Although his mainstream career spanned only four years, he is widely regarded as one of the most influential electric guitarists in the history of popular music, and one of the most celebrated musicians of the 20th century.', 'artists/jimi-hendrix.jpg', now(), now());
 insert into {{{prefix}}}artists (id, name, description, image, date_created, date_modified) values (252354, 'Phish', 'Phish is an American rock band noted for their musical improvisation, extended jams, blending of musical genres, and dedicated fan base. Formed at the University of Vermont in 1983, the band\'s four members- Trey Anastasio (guitars, lead vocals), Mike Gordon (bass, vocals), Jon Fishman (drums, percussion, vacuum, vocals), and Page McConnell (keyboards, vocals)- performed together for nearly 20 years before going on hiatus in August 2004. They reunited in March 2009 and have since resumed performing regularly. Phish\'s music blends elements of a wide variety of genres, including rock, progressive rock, psychedelic rock, hard rock, funk, folk and blues.', 'artists/phish.jpg', now(), now());
 insert into {{{prefix}}}artists (id, name, description, image, date_created, date_modified) values (251595, 'Iron Maiden', 'Iron Maiden are an English heavy metal band formed in Leyton, east London, in 1975 by bassist and primary songwriter Steve Harris. The band\'s discography has grown to thirty-seven albums, including fifteen studio albums, eleven live albums, four EPs, and seven compilations.', 'artists/iron-maiden.jpg', now(), now());
+
+# Ratatat Albums ##################################
+
+insert into {{{prefix}}}albums (id, title, image, pubyear, artist_id, date_created, date_modified) values (121826, 'Ratatat', 'albums/121826.jpg', 2004, 158120, now(), now());
+insert into {{{prefix}}}albums (id, title, image, pubyear, artist_id, date_created, date_modified) values (121827, 'Classics', 'albums/121827.jpg', 2006, 158120, now(), now());
+insert into {{{prefix}}}albums (id, title, image, pubyear, artist_id, date_created, date_modified) values (121831, 'LP3', 'albums/121831.jpg', 2008, 158120, now(), now());
+insert into {{{prefix}}}albums (id, title, image, pubyear, artist_id, date_created, date_modified) values (264782, 'LP4', 'albums/264782.jpg', 2010, 158120, now(), now());
+
+# Ratatat Songs ###################################
+
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Seventeen Years', 1, 121826, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('El Pico', 2, 121826, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Crips', 3, 121826, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Desert Eagle', 4, 121826, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Everest', 5, 121826, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Bustelo', 6, 121826, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Breaking Away', 7, 121826, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Lapland', 8, 121826, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Germany To Germany', 9, 121826, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Spanish Armada', 10, 121826, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Cherry', 11, 121826, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Montanita', 1, 121827, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Lex', 2, 121827, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Gettysburg', 3, 121827, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Wildcat', 4, 121827, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Tropicana', 5, 121827, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Loud Pipes', 6, 121827, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Kennedy', 7, 121827, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Swisha', 8, 121827, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Nostrand', 9, 121827, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Tacobel Canon', 10, 121827, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Shiller', 1, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Falcon Jab', 2, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Mi Viejo', 3, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Mirando', 4, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Flynn', 5, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Bird-Priest', 6, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Shempi', 7, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Imperials', 8, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Dura', 9, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('BruleÃ©', 10, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Mumtaz Khan', 11, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Gipsy Threat', 12, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Black Heroes', 13, 121831, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Bilar', 1, 264782, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Drugs', 2, 264782, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Neckbrace', 3, 264782, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('We Can\'t Be Stopped', 4, 264782, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Bob Gandhi', 5, 264782, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Mandy', 6, 264782, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Mahalo', 7, 264782, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Party With Children', 8, 264782, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Sunblocks', 9, 264782, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Bare Feast', 10, 264782, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Grape Juice City', 11, 264782, now(), now());
+insert into {{{prefix}}}songs (title, track_no, album_id, date_created, date_modified) values ('Alps', 12, 264782, now(), now());
+  
+  
